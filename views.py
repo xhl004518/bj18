78b1b1cf5ca725e5d6e5d98134ee81e7de4ab3eb
xhl004518 <59219367+xhl004518@users.noreply.github.com>
2020-09-14 20:52:56 +0800

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
 
 def index(request):
     return HttpResponse('ok')
+
+
+def login(request):
+    return redirect('/index')
